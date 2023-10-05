535331b7788d50d7486c3cbae6c879163c085f95
Steffen Kroggel <kroggel@rkw.de>
2023-10-05 16:36:41 +0200

    [FIX] Fixing false-positive of extensionScanner

--- Classes/Command/SendCommand.php
+++ Classes/Command/SendCommand.php
@@ -145,6 +145,7 @@ class SendCommand extends Command
                 str_replace(array("\n", "\r"), '', $e->getMessage())
             );
 
+            // @extensionScannerIgnoreLine
             $io->error($message);
             $this->getLogger()->log(LogLevel::ERROR, $message);
             $result = 1;
